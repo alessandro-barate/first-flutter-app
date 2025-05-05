fa7eadfd6fc3cc057f5fca521cdfefeda39ed673
alessandro-barate <alebarat@gmail.com>
2025-05-05 13:50:15 +0200

    Creating the gradient container class

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/gradient_container.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color.fromARGB(222, 0, 110, 236),
-                Color.fromARGB(255, 96, 110, 126),
-                Color.fromARGB(211, 45, 208, 116),
-              ],
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(color: Colors.white, fontSize: 30),
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
+  runApp(const MaterialApp(home: Scaffold(body: GradientContainer())));
 }

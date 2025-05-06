ac58d7986314c6b7784eb751344e55a8b54a64ad
alessandro-barate <alebarat@gmail.com>
2025-05-06 11:40:39 +0200

    File renamed

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/text_style.dart';
+import 'package:flutter_application_1/styled_text.dart';
 
 // Gradient Container
 class GradientContainer extends StatelessWidget {

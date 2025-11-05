e174f310b166d6e61099b29cd708f8db716b46e4
alessandro-barate <alebarat@gmail.com>
2025-11-05 11:38:53 +0100

    implementing the StatefulWidget

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,3 +1,41 @@
 import 'package:flutter/material.dart';
 
-class DiceRoller extends StatefulWidget {}
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var activeDiceImage = 'assets/images/dice-1.png';
+
+  void rollDice() {
+    setState(() {
+      activeDiceImage = 'assets/images/dice-4.png';
+    });
+    // print('Changing image...');
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(activeDiceImage, width: 200),
+        const SizedBox(height: 20),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            // padding: const EdgeInsets.only(top: 30), -> l'alternativa è usare, come sopra, un SizedBox
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text('Roll Dice!'),
+        ),
+      ],
+    );
+  }
+}
--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/dice_roller.dart';
 
 // Main text widget
 // import 'package:flutter_application_1/styled_text.dart';
@@ -8,16 +9,10 @@ const startAlignment = Alignment.topCenter;
 const endAlignment = Alignment.bottomCenter;
 
 // Gradient Container
-class GradientContainer extends StatefulWidget {
-  GradientContainer({super.key, required this.colors});
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key, required this.colors});
 
   final List<Color> colors;
-  var activeDiceImage = 'assets/images/dice-1.png';
-
-  void rollDice() {
-    activeDiceImage = 'assets/images/dice-4.png';
-    print('Changing image...');
-  }
 
   @override
   Widget build(context) {
@@ -29,24 +24,7 @@ class GradientContainer extends StatefulWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(activeDiceImage, width: 200),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                // padding: const EdgeInsets.only(top: 30), -> l'alternativa è usare, come sopra, un SizedBox
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 28),
-              ),
-              child: const Text('Roll Dice!'),
-            ),
-          ],
-        ),
-      ),
+      child: Center(child: DiceRoller()),
     );
   }
 }
